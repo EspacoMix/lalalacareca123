12087e34c359c6f9632333fcb6b427be9710c74c
EspaçoMix <105557405+EspacoMix@users.noreply.github.com>
2023-02-08 00:41:21 -0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,107 @@
+<link rel="stylesheet" href="css/footer.css">
+<footer class="text-center text-lg-start" style="color:white;background-color: #262626;">
+    
+    <!-- Grid container -->
+    <div class="container p-4">
+        
+        <!--Grid row-->
+        <div class="row">
+         
+            <!--Grid column-->
+            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+
+                <div class="tra">
+                    <h3>Navegue</h3>
+                </div>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" style="color:#fff;"aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" style="color:#fff;"aria-current="page"  href="cadastro.php">Cadastro</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" style="color:#fff;"aria-current="page" href="exibir.php">Exibir</a>
+                    </li>
+                </ul>
+            </div>
+            <!--Grid column-->
+
+            <!--Grid column-->
+            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                <div class="tra">
+                                                                <h3 id="contato">Contato</h3>
+                                                        </div>
+
+        <ul class="list-unstyled mb-0">
+          <li>
+            <a href="#!" class="text-light"> <div class="single-cta">
+                                                        <a class="ri-map-pin-2-line icon" id=""  style="color:#ff5e14;;" href="#"></a>
+                                                        <div class="cta-text">
+                                                                <h4>Vá para:</h4>
+                                                                <span class="text-light">
+                                                                    R. Des. Pfdsfdsfdsfd, 69 - Piranbu
+                                                                </span>
+                                                        </div></a>
+          </li>
+          <li>
+            <a href="#!" class="text-light"><div class="single-cta">
+                                                        <a class="ri-phone-line icon" id=""  style="color:#ff5e14;;" href="#"></a>
+                                                        <div class="cta-text">
+                                                                <h4>Ligue para:</h4>
+                                                                <span class="text-light">
+                                                                        (85) 9 98876-4948
+                                                                        <br>(85) 9 99756-4948
+                                                                </span>
+                                                        </div>
+                                                </div></a>
+          </li>
+          <li>
+            <a href="#!" class="text-light"><div class="single-cta">
+                                                        <a class="ri-mail-line icon" id=""  style="color:#ff5e14;;" href="#"></a>
+                                                        <div class="cta-text">
+                                                                <h4>Envie-nos um e-mail:</h4>
+                                                                <span class="text-light">espacomix@gmail.com</span>
+                                                        </div>
+                                                </div></a>
+          </li>
+
+        </ul>
+      </div>
+      <!--Grid column-->
+
+
+      <!--Grid column-->
+      <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+      <div class="tra">
+                                <h3>Parangaba</h3>
+                                                        </div>
+                                                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.1506509067244!2d-38.558110799999994!3d-3.7773818999999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c74ffb7dff0d15%3A0x533fe63aabab19d8!2sEspa%C3%A7o%20Mix!5e0!3m2!1spt-BR!2sbr!4v1672048242968!5m2!1spt-BR!2sbr" width="150" height="250" style="border-radius:10px;" allowfullscreen="" class="card-img-top" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                                            <div class="footer-social-icon">
+                                                                        <a class="ri-whatsapp-line icon" id="zap" href="https://wa.me/qr/D5Z5M43METFVG1" ></a>
+                                                                        <a class="ri-instagram-line icon" id="insta" href="https://instagram.com/espacomixparangaba"></a>
+                                                                        <a href="https://maps.app.goo.gl/dpZ9RM35KbdjUBes9"> <iconify-icon icon="mdi:google-my-business" class="icon" style="color: white;"></iconify-icon></a>
+                                                                </div>
+      </div>
+      <!--Grid column-->
+
+      
+    </div>
+    <!--Grid row-->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: #6D4B32;">
+  Copyright &copy; <span id="anoatual"></span> - Cordeiro - Todos os Direitos Reservados 
+    
+  </div>
+  <!-- Copyright -->
+</footer>
+<script>
+        const dataAtual = new Date();
+     const anoAtual = dataAtual. getFullYear();
+     document.getElementById('anoatual').innerHTML=anoAtual
+      
+      </script>
+      <script src="https://code.iconify.design/iconify-icon/1.0.2/iconify-icon.min.js"></script>
